199a47e0f8ea51c06439a9d9524798e38055bdf4
Samuel Nelson <samuel.h.nelson@protonmail.com>
2026-02-23 18:00:00 -0600

    editted brand design & about

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Solutions by Samuel
+            <span className="brand-highlight">Solutions </span> by Samuel
           </a>{" "}
         </div>
 
